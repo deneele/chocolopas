76af6a3a0ad0fda39b2679fa696553b217692a77
daniela-sys <daniela.arc.val@gmail.com>
2021-01-10 17:42:46 -0600

    Update chocolopas.js

--- scraping/chocolopas.js
+++ scraping/chocolopas.js
@@ -1,136 +1,54 @@
 const fs = require('fs');
 const path = require('path');
-module.exports = async (page, website) =>{
-    const {selectors} = website;
+module.exports = async (page, website) => {
+    const { selectors } = website;
     await page.goto(website.url);
     //Login
-        await page.waitForSelector('#Usuario');
-        await page.type('#Usuario', '4637B_01');
-        await page.waitForSelector('#contrasena');
-        await page.type('#contrasena', 'GugiGagr2686*');
-        await page.waitForSelector('#formularioLogIn > table > thead > tr:nth-child(5) > td > input');
-        await page.click('#formularioLogIn > table > thead > tr:nth-child(5) > td > input');
-    //Selecciona el link para mostrar los productos
-        await page.waitForSelector(selectors.trendsLink);
-        await page.click(selectors.trendsLink);
-        await page.goto("http://insta.ddns.net/installantas/ProductosCliente4/list?pagesize=-1");
-        await page.waitFor(1000); 
-        await page.waitForSelector(selectors.trendListTag);
-        const trendsText =  await page.evaluate((trendListTag) => {
-      const trendList = document.querySelectorAll(trendListTag);
-      //console.log(trendList);
-        const trendsText = [];
-      for (const trend of trendList) {
-          trendsText.push(trend.innerText);
-          //console.log(trend.innerText);
-          
-      }
-      return trendsText;
-  }, selectors.trendListTag);
-  //console.log(trendsText);
-  let content = [];
-  let columns = null;
-  const regExp = new RegExp('[A-z]+');
-  for (let text of trendsText) {
-      const trends = [];
-     // text = text.replace('\n', '');
-      //const textSplited = text.split('\t');
-     // const textSplited = text.split('\n').filter((txt) => regExp.test(txt));
-     text = text.replace(/\n/g,'\t');
-     columns = text.split(/\t/);
-      for (let iterator = 0; iterator < columns.length; iterator+=17) {
+    //select form to login
+    await page.waitForSelector('#navbar > ul > li:nth-child(7) > a');
+    await page.click('#navbar > ul > li:nth-child(7) > a');
+    //enter user
+    await page.waitForSelector('#Usuario');
+    await page.type('#Usuario', '4637B_01');
+    //enter pass
+    await page.waitForSelector('#contrasena');
+    await page.type('#contrasena', 'GugiGagr2686*');
+    //select botton "send"
+    await page.waitForSelector('#formularioLogIn > table > thead > tr:nth-child(5) > td > input');
+    await page.click('#formularioLogIn > table > thead > tr:nth-child(5) > td > input');
+    //select first brand
+    await page.waitForSelector('#tb_01 > tr > td:nth-child(2) > table > thead > tr:nth-child(2) > td > a');
+    await page.click('#tb_01 > tr > td:nth-child(2) > table > thead > tr:nth-child(2) > td > a');
+    //select all categories
+    await page.waitFor(1000);
+    await page.waitForSelector('#tb_01 > tr > td > table > thead > tr:nth-child(2) > td > a');
+    await page.click('#tb_01 > tr > td > table > thead > tr:nth-child(2) > td > a');
+    //select "muestra"
+    await page.waitForSelector('#tQuery_length > label > select')
+    await page.select('select[name="tQuery_length"]', '100');
+    // await page.waitForSelector('#tQuery');
+    // Selected table by aria-label instead of div id
+    const recordList = await page.$$eval('#tQuery', (trows) => {
+        let rowList = []
+        trows.forEach(row => {
+            let record = { 'id': '', 'stock': '', 'price': '' } //definimos el arreglo 
 
+            record.id = row.querySelector('td').innerText; // (tr < th < a) anchor tag text contains country name
+            const tdList = Array.from(row.querySelectorAll('td'), column => column.innerText); // getting textvalue of each column of a row and adding them to a list.
+            record.stock = tdList[7];
+            record.price = tdList[8];
+            if (tdList.length >= 0) {
+                rowList.push(record)
+            }
+        });
+        return rowList;
+    })
+    console.log(recordList);
+    fs.writeFileSync(
+        path.join(__dirname, `${website.scriptName}.json`),
+        JSON.stringify(recordList),
+        'utf8'
 
-        content.push({
-            sku: columns[iterator + 3],
-            name: "Llanta: " + columns[iterator + 4],
-            short_description: "Marca: " + columns[iterator + 9],
-            regular_price: columns[iterator + 14],
-            altura: columns[iterator + 7], 
-            ancho: columns[iterator + 6],
-            rin: columns[iterator + 8],
-            dimensions:{
-                length: '',
-                width:  'Ancho: ' + (columns[iterator + 6]),
-                height: 'Alto: '  + (columns[iterator + 7])
-            },
-            attributes:[{
-                id:4,
-            name:"Modelo",
-            position: 1,
-            visible: true,
-            variation: false,
-            options:[columns[iterator + 10]]
-         },
-        {
-            id:5,
-            name: "Rango",
-            position: 1,
-            visible: true,
-            variation: false,
-            options:[columns[iterator + 11]]
-        },
-        {
-            id:5,
-            name: "Rin",
-            position: 1,
-            visible: true,
-            variation: false,
-            options:[columns[iterator + 8]]
-        },
-        {
-            id:5,
-            name: "Tipo",
-            position: 1,
-            visible: true,
-            variation: false,
-            options:[columns[iterator + 5]]
-        },
-        {
-            id:6,
-            name:"Capas",
-            position: 1,
-            visible: true,
-            variation: false,
-            options:[columns[iterator +12]]
-        },
-        {
-            id: 7, 
-            name: "Piso", 
-            position: 1,
-            visible: true, 
-            variation: false, 
-            options: [columns[iterator + 13]]
-        },
-        {
-            id:9,
-            name:"Stock",
-            position: 1,
-            visible: true,
-            variation: false,
-            options:[ columns[iterator + 15]]
-        }],               
-        categories: [
-            {
-              id: 15
-            },
-        ],
-            images: [
-                {
-                
-                },
-              ]
-       })
-}
-
-console.log(content);
-
-}
-fs.writeFileSync(
-path.join(__dirname,`${website.scriptName}.json` ), 
-JSON.stringify(content),
-'utf8'
-
-);
+    );
 
 };
